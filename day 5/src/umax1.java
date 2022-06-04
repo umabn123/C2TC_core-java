52d592bcecc73f800a578912f6d541960a65b216
umabn123 <106304928+umabn123@users.noreply.github.com>
2022-06-04 10:39:22 +0530

    Update umax1.java

--- day 5/src/umax1.java
+++ day 5/src/umax1.java
@@ -5,7 +5,7 @@ int x;
 		umax1 myObj1 = new umax1();
 		umax1 myObj2 = new umax1();
 		myObj1.x = 24;
-		myObj2.x = 30;
+		myObj2.x = 25;
 		System.out.println(myObj1.x);
         System.out.println(myObj2.x);
 	}
