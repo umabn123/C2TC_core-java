bceb147e9e90a9f586a015511f78d3c0c327c56f
Kusuma <Kusuma@DESKTOP-D6PIEG7>
2022-06-04 10:27:17 +0530

    first commit

---
+++ day 5/src/umax1.java
@@ -0,0 +1,13 @@
+
+public class umax1 {
+int x;
+	public static void main(String[] args) {
+		umax1 myObj1 = new umax1();
+		umax1 myObj2 = new umax1();
+		myObj1.x = 24;
+		myObj2.x = 30;
+		System.out.println(myObj1.x);
+        System.out.println(myObj2.x);
+	}
+
+}
